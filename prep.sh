3676ec99206734bd7ec1f457c2e52173bc5af1b6
Mislav Blažević <krofnica996@gmail.com>
2017-07-20 15:17:06 +0200

    Run TreeCmp from prep.sh to calculate rf

--- prep.sh
+++ prep.sh
@@ -6,4 +6,9 @@ do
         cat "data$m/a$n" "data$m/a$a" >> "rfn$m"
     done
     sed -i "s/$/;/g" "rfn$m"
+    java -jar TreeCmp.jar -w 2 -d rf -i "rfn$m" -o "rfn$m.out" -P
+    rm "rfn$m"
+    b=$((m + 2))
+    ./filter "rfn$m.out" "dists$b"
+    rm "rfn$m.out"
 done
