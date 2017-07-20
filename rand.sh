b94a925aa20ff433d8eaa61766c989031cb06a13
Mislav Blažević <krofnica996@gmail.com>
2017-07-20 15:12:06 +0200

    Dont dump stderr into /dev/null. It seems that solver crashes on some instances, so we will need the log to figue out which ones.

--- rand.sh
+++ rand.sh
@@ -3,6 +3,6 @@ do
     for n in {0..9999}
     do
         a=$((n + 10000))
-        ./solver "data$m/a$n" "data$m/a$a" "data$m/s_a"$n"_a$a" 2>/dev/null >> "dists$m"
+        ./solver "data$m/a$n" "data$m/a$a" "data$m/s_a"$n"_a$a" 2>>rand.log >> "dists$m"
     done
 done
