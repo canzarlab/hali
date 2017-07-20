905ddf17a2029d1c369cdec6d0909d5114ca2d99
Mislav Blažević <krofnica996@gmail.com>
2017-07-20 11:40:16 +0200

    Add utility to parse TreeCmp output

---
+++ filter.cpp
@@ -0,0 +1,27 @@
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+bool input(ifstream& ifs, string& in)
+{
+    for (int i = 0; i < 7; ++i)
+        if (!(ifs >> in))
+            return false;
+    return true;
+}
+
+int main(int argc, char** argv)
+{
+    if (argc != 3)
+    {
+        cout << "usage: " << argv[0] << " <input> <output>\n";
+        return EXIT_FAILURE;
+    }
+    
+    ifstream ifs(argv[1]);
+    ofstream ofs(argv[2]);
+    string in;
+    getline(ifs, in);
+    while (input(ifs, in))
+        ofs << in << ' ';
+}
