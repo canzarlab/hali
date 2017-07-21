5049183aa26dae41beff3933ada5b0ccbc5ba584
Mislav Blažević <krofnica996@gmail.com>
2017-07-21 12:49:45 +0200

    Fix some bugs in rand.sh

--- rand.sh
+++ rand.sh
@@ -8,17 +8,17 @@ done
 ./bgen 50 20000
 for m in {0..1}
 do
+    b=$((m+2))
+    rm -f "dists$b"
+    rm -f "rfm$m"
     for n in {0..9999}
     do
         a=$((n + 10000))
         ./phylo2tc -t1 "data$m/a$n" -t2 "data$m/a$a" -k 1 >& /dev/null
-        b=$((m+2))
         mv t1mod "data$b/a$n"
         mv t2mod "data$b/a$a"
         mv sim_t1_t2 "data$b/s_a"$n"_a$a"
-        rm -f "dists$b"
-        ./solver "data$m/a$n" "data$m/a$a" "data$m/s_a"$n"_a$a" 2 >> rand.log >> "dists$b"
-        rm -f "rfm$m"
+        ./solver "data$m/a$n" "data$m/a$a" "data$m/s_a"$n"_a$a" 2>>rand.log >> "dists$b"
         cat "data$m/a$n" "data$m/a$a" >> "rfn$m"
     done
     sed -i "s/$/;/g" "rfn$m"
