edc00f5655995aed0b0cf6fac36ff17ea1d0e819
Mislav Blažević <krofnica996@gmail.com>
2017-07-24 15:01:34 +0200

    Generate alignments and run for different ks

--- rand.sh
+++ rand.sh
@@ -1,6 +1,6 @@
-if [ "$#" -ne 3 ]
+if [ "$#" -ne 2 ]
 then
-    echo "usage: <n-leaves> <n-tree-pairs> <k>"
+    echo "usage: <n-leaves> <n-tree-pairs>"
     exit 1
 fi
 for m in {0..3}
@@ -19,18 +19,21 @@ do
     for ((n=0; n<$2; n++))
     {
         a=$((n+$2))
-        ./phylo2tc -t1 "data$m/a$n" -t2 "data$m/a$a" -k $3 >& /dev/null
-        mv t1mod "data$b/a$n"
-        mv t2mod "data$b/a$a"
-        mv sim_t1_t2 "data$b/s_a"$n"_a$a"
-        ./solver "data$b/a$n" "data$b/a$a" "data$b/s_a"$n"_a$a" 2>>rand.log >> "dists$b"
+        for k in 1 5 10 20
+        do
+            ./phylo2tc -t1 "data$m/a$n" -t2 "data$m/a$a" -k $k >& /dev/null
+            mv t1mod "data$b/a$n"
+            mv t2mod "data$b/a$a"
+            mv sim_t1_t2 "data$b/s_a"$n"_a$a"
+            ./solver "data$b/a$n" "data$b/a$a" "data$b/s_a"$n"_a$a" 2>>rand.log >> "dists"$b"k$k"
+        done
         cat "data$m/a$n" "data$m/a$a" >> "rfn$m"
     }
     sed -i "s/://g" "rfn$m"
     sed -i "s/$/;/g" "rfn$m"
     for d in mc rc ns tt mp
     do
-        java -jar bin/TreeCmp.jar -w 2 -d $d -i "rfn$m" -o $d"n$m.out"
+        java -jar bin/TreeCmp.jar -w 2 -d $d -i "rfn$m" -o $d"n$m.out" -A
         ./filter $d"n$m.out" $d"dists$m"
         rm $d"n$m.out"
     done
