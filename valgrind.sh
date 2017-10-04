86dcdd8df4e9001d2db23f5c40d6b9d7cb3c095c
Mislav Blažević <mislav.blazevic5@gmail.com>
2017-10-04 13:26:21 +0200

    Update valgrind.sh

--- valgrind.sh
+++ valgrind.sh
@@ -1,3 +1,3 @@
 make
-valgrind --tool=memcheck --leak-check=full ./solver data/t1mod_d data/t2mod_d data/sim_t1_t2
+valgrind --tool=memcheck --leak-check=full ./solver dag1 map1 dag2 map2 align 2 j 1
 rm solver
