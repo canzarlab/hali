2f13c82ca18ccc9ed425e8ea2a973e4766e7aa8e
Mislav Blažević <mislav.blazevic5@gmail.com>
2018-03-30 20:32:06 +0200

    Add bunch of gpl notices

--- Constraint.cpp
+++ Constraint.cpp
@@ -1,3 +1,13 @@
+/*
+    Copyright (C) 2018 Mislav Blažević
+
+    This file is part of Hali.
+
+    Hali is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+*/
 #include "Constraint.h"
 
 Constraint::Constraint(vector<ET>& Triplets, Graph& t1, Graph& t2, vvi& K, Vector& x, bool swp) : Triplets(Triplets), t1(t1), t2(t2), K(K), x(x), swp(swp)
--- LPInt.cpp
+++ LPInt.cpp
@@ -1,3 +1,13 @@
+/*
+    Copyright (C) 2018 Mislav Blažević
+
+    This file is part of Hali.
+
+    Hali is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+*/
 #include "LPInt.h"
 #include "Timer.h"
 #include <iostream>
