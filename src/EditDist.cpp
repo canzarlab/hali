aa1380541f90d7aca080facb7e4e8ac506fb83e5
Mislav Blažević <mislav.blazevic5@gmail.com>
2018-11-02 13:03:34 +0100

    Use an array instead of map for storing dp solution
    Drastically improves performance and reduces memory usage

--- src/EditDist.cpp
+++ src/EditDist.cpp
@@ -22,7 +22,15 @@ typedef vector<vvvi> vvvvi;
 typedef vector<string> vs;
 typedef vector<vs> vvs;
 typedef tuple<int, int, int, int> i4;
-typedef map<i4, pair<vector<i4>, int>> bt_map;
+
+enum
+{
+    INS_TREE = -2,
+    DEL_TREE = -1,
+    MATCH = 0,
+    DEL_NODE = 1,
+    INS_NODE = 2,
+};
 
 int GetNumTrees(newick_node* root, vn& N)
 {
@@ -88,16 +96,16 @@ int CalcSubtreeSizes(newick_node* root, map<newick_node*, int>& S)
     return S[root] = s;
 }
 
-int Distance(vvvvi& DP, bt_map& P, vvi& T1, vvi& T2, vvs& L1, vvs& L2, int n, int m, int i, int j, int k, int l)
+int Distance(vvvvi& DP, vvvvi& P, vvi& T1, vvi& T2, vvs& L1, vvs& L2, int n, int m, int i, int j, int k, int l)
 {
     if (i + j == n)
     {
-        P[{i, j, k, l}] = {{}, -2};
+        P[i][j][k][l] = INS_TREE;
         return m - k - l;
     }
     if (k + l == m)
     {
-        P[{i, j, k, l}] = {{}, -1};
+        P[i][j][k][l] = DEL_TREE;
         return n - i - j;
     }
 
@@ -116,11 +124,11 @@ int Distance(vvvvi& DP, bt_map& P, vvi& T1, vvi& T2, vvs& L1, vvs& L2, int n, in
     int d = L1[i][j + 1] != L2[k][l + 1];
     int mm = min(min(o, a), b + c + d);
     if (mm == b + c + d)
-        P[{i, j, k, l}] = {{{x, y, xx, yy}, {i, j + T1[i][j + 1], k, l + T2[k][l + 1]}}, 0};
+        P[i][j][k][l] = MATCH;
     else if (mm == o)
-        P[{i, j, k, l}] = {{{i, j + 1, k, l}}, 1};
+        P[i][j][k][l] = DEL_NODE;
     else
-        P[{i, j, k, l}] = {{{i, j, k, l + 1}}, 2};
+        P[i][j][k][l] = INS_NODE;
     return mm;
 }
 
@@ -155,7 +163,7 @@ void GenTables(newick_node* root, int n, vvs& L, vvi& T)
                 L[i][j] = node->taxon, T[i][j] = S[node];
 }
 
-int OrderedEditDist(bt_map& P, vvs& L1, vvs& L2, vvi& T1, vvi& T2, int n, int m)
+int OrderedEditDist(vvvvi& P, vvs& L1, vvs& L2, vvi& T1, vvi& T2, int n, int m)
 {
     vvvvi DP(n + 1, vvvi(n + 1, vvi(m + 1, vi(m + 1, -1))));
     for (int i = n; i >= 0; --i)
@@ -175,36 +183,46 @@ void DeallocTree(newick_node* root)
     delete root;
 }
 
-void PrintMatching(bt_map& BP, vvs& L1, vvs& L2, i4 r, bool swp)
+void PrintMatching(vvvvi& BP, vvs& L1, vvs& L2, vvi& T1, vvi& T2, i4 r, bool swp)
 {
-    int i, j, k, l, op = BP[r].second;
+    int i, j, k, l;
     tie(i, j, k, l) = r;
     int n = L1.size() - 1, m = L2.size() - 1;
-    switch (op)
+    switch (BP[i][j][k][l])
     {
-        case -2:
+        case INS_TREE:
             for (int nl = l + 1; nl <= m - k; ++nl)
                 cout << (!swp ? "INS " : "DEL ") << L2[k][nl] << '\n';
             break;
-        case -1:
+        case DEL_TREE:
             for (int nj = j + 1; nj <= n - i; ++nj)
                 cout << (!swp ? "DEL " : "INS ") << L1[i][nj] << '\n';
             break;
-        case 0:
+        case MATCH:
         {
-            string x = L1[i][j + 1], y = L2[k][l + 1];
-            cout << "MATCH " << (swp ? y : x) << ' ' << (swp ? x : y) << '\n';
+            string xs = L1[i][j + 1], ys = L2[k][l + 1];
+            int p = n - T1[i][j + 1] - j, q = m - T2[k][l + 1] - l;
+            int x = i ? p + 1 : p + 2, y = i ? j : j - 1;
+            int xx = k ? q + 1 : q + 2, yy = k ? l : l - 1;
+            if (i == 0 && j == 0)
+                x = 1, y = 0;
+            if (k == 0 && l == 0)
+                xx = 1, yy = 0;
+
+            cout << "MATCH " << (swp ? ys : xs) << ' ' << (swp ? xs : ys) << '\n';
+            PrintMatching(BP, L1, L2, T1, T2, {i, j + T1[i][j + 1], k, l + T2[k][l + 1]}, swp);
+            PrintMatching(BP, L1, L2, T1, T2, {x, y, xx, yy}, swp);
             break;
         }
-        case 1:
+        case DEL_NODE:
             cout << (!swp ? "DEL " : "INS ") << L1[i][j + 1] << '\n';
+            PrintMatching(BP, L1, L2, T1, T2, {i, j + 1, k, l}, swp);
             break;
-        case 2:
+        case INS_NODE:
             cout << (!swp ? "INS " : "DEL ") << L2[k][l + 1] << '\n';
+            PrintMatching(BP, L1, L2, T1, T2, {i, j, k, l + 1}, swp);
             break;
     }
-    for (i4 p : BP[r].first)
-        PrintMatching(BP, L1, L2, p, swp);
 }
 
 void EditDist(Graph& g1, Graph& g2)
@@ -219,8 +237,9 @@ void EditDist(Graph& g1, Graph& g2)
     vvvi P;
     vvi S;
     GenPerms(n2, 0, S, P);
-    bt_map BP;
+    vvvvi BP;
     vvs BL1, BL2;
+    vvi BT1, BT2;
     int mm = numeric_limits<int>::max();
     for (int i = 0; i < P.size(); ++i)
     {
@@ -231,12 +250,12 @@ void EditDist(Graph& g1, Graph& g2)
         vvi T1(n + 1, vi(n + 1, 1)), T2(m + 1, vi(m + 1, 1));
         GenTables(t1.GetRoot(), n, L1, T1);
         GenTables(t2.GetRoot(), m, L2, T2);
-        bt_map NP;
+        vvvvi NP(n + 1, vvvi(n + 1, vvi(m + 1, vi(m + 1, -1))));
         int nm = OrderedEditDist(NP, L1, L2, T1, T2, n, m);
         if (nm < mm)
-            mm = nm, BP = NP, BL1 = L1, BL2 = L2;
+            mm = nm, BP = NP, BL1 = L1, BL2 = L2, BT1 = T1, BT2 = T2;
         DeallocTree(nroot);
     }
-    PrintMatching(BP, BL1, BL2, {0, 0, 0, 0}, swp);
+    PrintMatching(BP, BL1, BL2, BT1, BT2, {0, 0, 0, 0}, swp);
     clog << "DIST: " << mm << endl;
 }
